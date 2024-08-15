2066b194c0e8b1154824f65e652c1402021a7ef9
danielMitkov <daniel.mitkov01@gmail.com>
2024-08-16 01:47:22 +0300

    added azure storage acc

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragedaniel"
+    container_name       = "taskboardstoragecontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
